06701f3c8221c82f70b2fffcef1421902a67d50a
vietmrvu <viet.mrvu@gmail.com>
2024-05-21 21:01:43 +0700

    first commit

--- autism/settings.py
+++ autism/settings.py
@@ -77,7 +77,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-            'corsheaders.middleware.CorsMiddleware',
 
 ]
 ROOT_URLCONF = 'autism.urls'
